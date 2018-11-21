e8d48d9f60eb06d5f30cfaecd4007c0af7e5ef2b
Erlis Vidal <erlis.vidal@gmail.com>
2018-11-21 11:51:26 -0500

    Initial commit

---
+++ Consumer/Actors.fs
@@ -0,0 +1,56 @@
+﻿module Actors
+
+open Akka.FSharp
+open Akka.Actor
+
+// ............. Messages ...................... // 
+type MasterMessages = 
+    | RegisterWorker 
+    | UnregisterWorker 
+    | GimmeWork
+
+type WorkerMessages  =
+    | WorkAvailable
+    | Work
+
+// ............. Actors ...................... // 
+let masterActor (mailbox : Actor<_>) = 
+    let rec loop workers = actor {
+        let! message = mailbox.Receive () 
+        match message with 
+        | RegisterWorker -> 
+            let current = mailbox.Sender ()
+            let workersWithoutCurrent = workers |> List.filter(fun x -> x <> current)
+            return! loop (current::workersWithoutCurrent)
+        
+        | UnregisterWorker -> 
+            let current = mailbox.Sender () 
+            let workersWithoutCurrent = workers |> List.filter(fun x -> x <> current)
+            return! loop workersWithoutCurrent
+
+        | GimmeWork -> 
+            printf "TODO: Pending Give work to worker"
+
+        return! loop workers
+    }
+    loop [] 
+
+
+let workerActor (master: IActorRef) (mailbox: Actor<_>) =
+    // pre-start 
+    master <! RegisterWorker 
+    master <! GimmeWork
+    
+    // this registers a function to be called on post-stop
+    mailbox.Defer (fun () -> master <! UnregisterWorker)
+
+    let rec loop () = actor {
+        let! message = mailbox.Receive () 
+        match message with 
+        | WorkAvailable -> master <! GimmeWork
+        | Work -> printf "TODO: Pending processing Work!!!"
+        
+        return! loop()
+    }
+    loop () 
+
---
+++ Consumer/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open Akka.FSharp
+open Actors
+
+[<EntryPoint>]
+let main argv = 
+    let system = System.create "system" <| Configuration.load () 
+    let masterRef = spawn system "masterActor" masterActor 
+    0 // return an integer exit code
